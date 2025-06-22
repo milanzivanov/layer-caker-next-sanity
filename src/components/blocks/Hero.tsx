687b13cbf5e2eb1865b970cd9ab8ba1514926eed
milan <milanzivanov@users.noreply.github.com>
2025-06-22 16:08:34 +0200

    sanity/base removed

--- src/components/blocks/Hero.tsx
+++ src/components/blocks/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero({ title, text, image }: HeroProps) {
   return (
     <section className="isolate w-full aspect-[2/1] py-16 overflow-hidden">
       <div className="relative flex flex-col justify-center items-center gap-8 h-full z-20">
-        <div className="flex flex-col gap-4 items-center bg-black/50 p-8 mb-[200px]">
+        <div className="flex flex-col gap-4 items-center bg-blue-400/20 p-8 mb-[200px]">
           {title ? (
             <h1 className="text-2xl md:text-6xl font-bold text-slate-100 uppercase text-pretty max-w-5xl">
               {title}
